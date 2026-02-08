8d2f64e0d82ebaa34dc9f0987d0ef57ae74f7b5c
Phillip Thapa <1thapat@gmail.com>
2026-02-08 18:49:09 -0500

    Succesfully fetches countries from api

--- src/models/Countries.ts
+++ src/models/Countries.ts
@@ -1,3 +1,5 @@
+const api: string = "https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital,cca3";
+
 interface Country {
   name: {
     common: string;
@@ -13,3 +15,16 @@ interface Country {
   population?: number;
 }
 
+async function fetchCountries(): Promise<Country[]> {
+  const response = await fetch(api);
+  const countries = await response.json();
+  return countries;
+}
+
+const fetchedCountries = await fetchCountries();
+
+console.log(fetchedCountries.length);
+
+fetchedCountries.forEach((country) => {
+  console.log(country);
+});
