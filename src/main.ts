0aa3422d2a27cf4484459cbba756286afc519547
Phillip Thapa <1thapat@gmail.com>
2026-02-08 19:59:11 -0500

    added comments to the README.md file and also imported class and function to the main .ts file

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
+import { fetchCountries } from "./models/Countries.js";
+import type { Country } from "./models/Countries.js";
 
+const renderCountries = async (Countries: Country[]) => {
+  const container = document.getElementById("countries-container");
+  if (!container) return;
+
+//   now this will loop into each countryy
+}
+
+renderCountries().then((countries) => {
+  console.log(countries);
+});
--- src/models/Countries.ts
+++ src/models/Countries.ts
@@ -1,6 +1,6 @@
 const api: string = "https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital,cca3";
 
-interface Country {
+export interface Country {
   name: {
     common: string;
     official: string;       
@@ -15,16 +15,10 @@ interface Country {
   population?: number;
 }
 
-async function fetchCountries(): Promise<Country[]> {
+export async function fetchCountries(): Promise<Country[]> {
   const response = await fetch(api);
   const countries = await response.json();
   return countries;
 }
 
-const fetchedCountries = await fetchCountries();
 
-console.log(fetchedCountries.length);
-
-fetchedCountries.forEach((country) => {
-  console.log(country);
-});
