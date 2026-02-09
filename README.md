a1d81eb1918c616c4a25272289ecdbc3b7b6d729
Phillip Thapa <1thapat@gmail.com>
2026-02-08 20:23:42 -0500

    created elements and assigned value to them in main.js file inside the main loop

---
+++ README.md
@@ -0,0 +1,6 @@
+First, I created folder and files using bash command lines.
+Countries.ts - contains Country interface for the blueprint contract but here is used just to fetch data from api type Country.
+
+index.html - contain header - where in the worlds and button for changing mode for the page. It also contain search button and filer by region select options.And countries-container div where countries will be rendered.
+
+main.ts - as of now this will import countries and country ts. and creates and adds element to the html file
--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,38 @@ const renderCountries = async (Countries: Country[]) => {
   const container = document.getElementById("countries-container");
   if (!container) return;
 
-//   now this will loop into each countryy
+//   now this will loop into each country
+  Countries.forEach((country) => {
+
+    //creates card dev
+   const card = document.createElement("div");
+   card.className ="country-card";
+
+   //creates and sets flag element
+   const flag = document.createElement("img");
+   flag.src = country.flags.png;
+   flag.alt = country.flags.alt;
+
+   //creates info dev
+   const info = document.createElement("div");
+   info.className = "country-info";
+
+   //creates country name and sets it to the country's common name
+   const name = document.createElement("h2");
+   name.textContent = country.name.common;
+   
+   const population = document.createElement("p");
+   population.innerHTML = country.population?.toLocaleString() || "N/A";
+
+   const region = document.createElement("p");
+   region.innerHTML = country.region || "N/A";
+
+   const capital = document.createElement("p");
+   capital.textContent = country.capital || "N/A";
+
+
+
+
+  });
 }
 
-renderCountries().then((countries) => {
-  console.log(countries);
-});
